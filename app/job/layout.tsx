96970d19fc8285cbb9c575a112e17b568a7a4bb6
Kkarthi123 <karthikeyankengg@gmail.com>
2025-09-24 22:03:43 +0530

    not found page implemented

--- app/job/layout.tsx
+++ app/job/layout.tsx
@@ -4,7 +4,7 @@ export default function JobLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="sticky top-0 z-50 bg-white">
-      <NavBar />
+        <NavBar />
       </div>
       {children}
 
--- app/job/new-job/page.tsx
+++ app/job/new-job/page.tsx
@@ -1,8 +1,3 @@
-'use client';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { v4 as uuidv4 } from 'uuid';
-import { useJobstore } from '@/store/jobStore';
 import JobForm from '@/components/JobForm';
 
 export default function AddJob() {
